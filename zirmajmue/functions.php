91836877e23cbc1ddd7514d3014776c7e03cb8f3
omidrm99 <omidrm99@gmail.com>
2025-01-21 19:32:19 +0330

    functions added

--- zirmajmue/functions.php
+++ zirmajmue/functions.php
@@ -84,3 +84,134 @@ function generateRandomString(): string
     }
     return $randomString;
 }
+function sendPhoto(int $chat_id, string $photo, string $caption = null, string $parse_mode = null, array $caption_entities = null, bool $has_spoiler = null, bool $disable_notification = null, bool $protect_content = true, int $reply_to_message_id = null, bool $allow_sending_without_reply = null, $reply_markup = null): bool|string
+{
+    $params = [
+        'chat_id' => $chat_id,
+        'photo' => $photo
+    ];
+
+    foreach (
+        [
+            'caption' => $caption,
+            'parse_mode' => $parse_mode,
+            'caption_entities' => $caption_entities,
+            'has_spoiler' => $has_spoiler,
+            'disable_notification' => $disable_notification,
+            'protect_content' => $protect_content,
+            'reply_to_message_id' => $reply_to_message_id,
+            'allow_sending_without_reply' => $allow_sending_without_reply,
+            'reply_markup' => $reply_markup
+        ] as $key => $value
+    ) {
+        if ($value !== null) {
+            $params[$key] = $value;
+        }
+    }
+
+    return bot('sendPhoto', $params);
+}
+function editMessageText(int|string $chat_id = null, int $message_id = null, string $inline_message_id = null, string $text = null, string $parse_mode = null, array $entities = null, bool $disable_web_page_preview = null, $reply_markup = null): void
+{
+    $params = [
+        'chat_id' => $chat_id
+    ];
+
+    foreach (
+        [
+            'message_id' => $message_id,
+            'inline_message_id' => $inline_message_id,
+            'text' => $text,
+            'parse_mode' => $parse_mode,
+            'entities' => $entities,
+            'disable_web_page_preview' => $disable_web_page_preview,
+            'reply_markup' => $reply_markup
+        ] as $key => $value
+    ) {
+        if ($value !== null) {
+            $params[$key] = $value;
+        }
+    }
+
+    bot('editMessageText', $params);
+}
+function editMessageCaption(int|string $chat_id = null, int $message_id = null, string $inline_message_id = null, string $caption = null, string $parse_mode = null, array $caption_entities = null, $reply_markup = null): void
+{
+    $params = [
+        'chat_id' => $chat_id
+    ];
+
+    foreach (
+        [
+            'message_id' => $message_id,
+            'inline_message_id' => $inline_message_id,
+            'caption' => $caption,
+            'parse_mode' => $parse_mode,
+            'caption_entities' => $caption_entities,
+            'reply_markup' => $reply_markup
+        ] as $key => $value
+    ) {
+        if ($value !== null) {
+            $params[$key] = $value;
+        }
+    }
+
+    bot('editMessageCaption', $params);
+}
+function answerCallbackQuery(string $callback_query_id = null, string $text = null, bool $show_alert = null, string $url = null, int $cache_time = null): void
+{
+    $params = [
+        'callback_query_id' => $callback_query_id
+    ];
+
+    foreach (
+        [
+            'text' => $text,
+            'show_alert' => $show_alert,
+            'url' => $url,
+            'cache_time' => $cache_time
+        ] as $key => $value
+    ) {
+        if ($value !== null) {
+            $params[$key] = $value;
+        }
+    }
+
+    bot('answerCallbackQuery', $params);
+}
+function setStep(string $step): bool|mysqli_result
+{
+    global $db;
+    global $from_id;
+    return $db->query("UPDATE `users` SET `step` = '$step' WHERE `user_id`= '$from_id'");
+}
+function getStep(int $user_id)
+{
+    global $db;
+    return $db->query("SELECT `step` FROM `users` WHERE `user_id`= '$user_id'")->fetch_object()->step;
+}
+
+function deleteMessage($chat_id, $message_id): void
+{
+    $params = [
+        'chat_id' => $chat_id,
+        'message_id' => $message_id
+    ];
+    bot('deleteMessage', $params);
+}
+function getChatMember(int $chat_id): string|bool
+{
+
+    $data = bot(
+        'getChatMember',
+        [
+            'chat_id' => $chat_id,
+            'user_id' => $GLOBALS['from_id']
+        ]
+    );
+    if ($data['result']['status']) {
+        return $data['result']['status'];
+    } else {
+        return true;
+    }
+}
