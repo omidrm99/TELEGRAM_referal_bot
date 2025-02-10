f48b412600be84b917c4d6048814f56a339c8139
omidrm99 <omidrm99@gmail.com>
2025-02-10 11:24:53 +0330

    fix: update message formatting to use Markdown for admin notifications

--- zirmajmue/step/support.php
+++ zirmajmue/step/support.php
@@ -11,10 +11,10 @@ $msg = 'پیام شما ارسال شد
 تا 24 ساعت بهتون جواب میدیم';
 $msg_admin = "یک پیام پشتیبانی ارسال شده
 متن پیام : {$text}
-آیدی عددی فرستنده : {$from_id}";
+آیدی عددی فرستنده : `{$from_id}`";
 
 
-sendMessage($bot_admins[0], $msg_admin);
+sendMessage($bot_admins[0], $msg_admin,parse_mode:'Markdown');
 sendMessage($from_id, $msg, reply_markup: $keyboard_home);
 setStep('home');
 die;
