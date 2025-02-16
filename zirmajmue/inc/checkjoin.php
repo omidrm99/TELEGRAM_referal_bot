be391e89ce648ded271ed89849965f9bcffb7bb8
omidrm99 <omidrm99@gmail.com>
2025-02-16 19:40:34 +0330

    refactor: simplify chat member status check and remove unused function

--- zirmajmue/inc/checkjoin.php
+++ zirmajmue/inc/checkjoin.php
@@ -1,33 +1,21 @@
 <?php
 
+$chat_id = -1002332898108;
 
-
-// -1002332898108
 $checkjoin = bot(
     'getChatMember',
-    [
-        'chat_id' =>   -1002332898108,
-        'user_id' => $from_id
+[
+    'chat_id' => $chat_id,
+    'user_id' => $from_id
     ]
 );
 
+$status = json_decode($checkjoin, true)['result']['status'];
 
-function decodeJson($jsonString)
-{
-    // Decode the JSON string
-    $decodedArray = json_decode($jsonString, true);
-    // Access the 'status' field
-    $status = $decodedArray['result']['status'];
-    return $status;
-}
-
-$checkjoin1 = decodeJson($checkjoin);
-
-if ($checkjoin1 == 'left') {
-    debug('inside if');
-    die;
+if ($status == 'left') {
     $msg = 'برای ادامه در کانال های زیر عضو شوید';
     sendMessage($from_id, $msg);
 }
+
 debug('end of code');
 die;
